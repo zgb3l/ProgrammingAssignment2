7248af2e374b90c4303867ee3cbe4c3687039d06
zgb3l <zgb3l@outlook.com>
2021-12-03 19:16:12 +0100

    making edits more readable

--- cachematrix.R
+++ cachematrix.R
@@ -16,9 +16,20 @@
 
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) m <<- inverse ## replace mean with inverse
+  getInverse <- function() m
+  list(set = set, 
+       get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 
 }
-
 
 ## Write a short comment describing the cacheSolve function:
 ## Computing the inverse of a square matrix can be done with the solve
@@ -26,6 +37,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## solve(x) returns its inverse. So, cacheSolve() computes the inverse of
 ## a matrix.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) { ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse() ## replace getmean with getinverse
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data) ## replace mean with solve
+  x$setInverse(m)
+  m
 }
